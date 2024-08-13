78a2f7475f199da2a9c0cc46fabbacaf432a0a0e
nokonoko1203 <healtheworld.1203@gmail.com>
2024-08-13 17:21:07 +0900

    add debug info

--- src/texture.rs
+++ src/texture.rs
@@ -143,19 +143,25 @@ impl CroppedTexture {
 
     pub fn crop(&self, image: &DynamicImage) -> DynamicImage {
         let (x, y) = self.origin;
+        if x + self.width > image.width() || y + self.height > image.height() {
+            panic!(
+                "The cropped area is out of bounds. x: {}, y: {}, width: {}, height: {}, image_path: {:?}",
+                x, y, self.width, self.height, self.image_path
+            );
+        }
         let cropped_image = image.view(x, y, self.width, self.height).to_image();
 
-        // remove transparent area
-        let mut clipped = ImageBuffer::new(self.width, self.height);
-        for (px, py, pixel) in cropped_image.enumerate_pixels() {
-            let uv = (
-                px as f64 / self.width as f64,
-                1.0 - py as f64 / self.height as f64,
-            );
-            if is_point_inside_polygon(uv, &self.cropped_uv_coords) {
-                clipped.put_pixel(px, py, *pixel);
-            }
-        }
+        // // remove transparent area
+        // let mut clipped = ImageBuffer::new(self.width, self.height);
+        // for (px, py, pixel) in cropped_image.enumerate_pixels() {
+        //     let uv = (
+        //         px as f64 / self.width as f64,
+        //         1.0 - py as f64 / self.height as f64,
+        //     );
+        //     if is_point_inside_polygon(uv, &self.cropped_uv_coords) {
+        //         clipped.put_pixel(px, py, *pixel);
+        //     }
+        // }
 
         // let (min_x, min_y, max_x, max_y) = find_non_transparent_bounds(&clipped);
 
